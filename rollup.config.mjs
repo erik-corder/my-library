3d76b359d78db4ed1606368de774e0c748da8e68
erik <isurus@digital400.com>
2024-07-26 12:52:48 +0530

    commit

---
+++ rollup.config.mjs
@@ -0,0 +1,39 @@
+const nodeResolve = require("@rollup/plugin-node-resolve");
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript = require("@rollup/plugin-typescript");
+const dts = require("rollup-plugin-dts").default;
+
+const resolve = require("@rollup/plugin-node-resolve");
+
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: {
+      file: "dist/index.d.ts",
+      format: "esm",
+    },
+    plugins: [dts()],
+  },
+];
